45da4542c6cd6047689bc41ee0a1a4447dcf86be
Anusigan Sivananthan <148673260+Anusigan@users.noreply.github.com>
2025-01-31 09:10:34 +0530

    Algo tutorials

--- src/findMultiples/FindMultiples.java
+++ src/findMultiples/FindMultiples.java
@@ -1,12 +1,13 @@
+package findMultiples;
+
+// This java program i s designed to find duplicates and triplicates in an array of integers and to measure the time taken to find them
 
 
-
+//A dynamic array implementation
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Random;
 
 /**
- *
  * @author klaus
  */
 public class FindMultiples {
@@ -15,30 +16,30 @@ public class FindMultiples {
      *Find an index where a value in the array occurs for the second time
      * Returns -1 if no such index exists
      */
-    public static int findDuplicate(ArrayList<Integer> numbers){
-        for(int i = 0; i < numbers.size(); i++)
-            for(int j = 0; j < i; j++)
-                if(numbers.get(i) == numbers.get(j))
+    public static int findDuplicate(ArrayList<Integer> numbers) {
+        for (int i = 0; i < numbers.size(); i++)
+            for (int j = 0; j < i; j++)
+                if (numbers.get(i) == numbers.get(j))
                     return i;
-	return -1;  // Failure case
+        return -1;  // Failure case
     }
 
     /*
      * Find an index where a value in the array occurs for the third time
      * Returns -1 if no such index exists
      */
-    public static int findTriplicate(ArrayList<Integer> numbers){
+    public static int findTriplicate(ArrayList<Integer> numbers) {
         // TO DO
         return -1;  // Failure case
     }
 
-    public static ArrayList<Integer> createInput(int size, int repeats, boolean shuffle){
+    public static ArrayList<Integer> createInput(int size, int repeats, boolean shuffle) {
         ArrayList<Integer> result = new ArrayList<>(size);
         for (int i = repeats; i < size; i++)
             result.add(i);
-        for(int i = 0; i < repeats; i++)
+        for (int i = 0; i < repeats; i++)
             result.add(0);
-        if (shuffle) 
+        if (shuffle)
             Collections.shuffle(result);
         return result;
     }
@@ -48,24 +49,24 @@ public class FindMultiples {
      */
     public static void main(String[] args) {
         // How many values to generate
-        int numValues = 100000; 
-    // Whether the input should be shuffled
+        int numValues = 100000;
+        // Whether the input should be shuffled
         boolean shuffle = true;
         // Whether to look for triplicate values
         boolean triplicates = false;
         // Whether to print data. Only use with small numbers of values.
-        boolean printData = false; 
-        
+        boolean printData = false;
+
         // Create some data, either sorted or unsorted
         ArrayList<Integer> data = createInput(numValues, (triplicates ? 3 : 2), shuffle);
         // Optionally print the data (to check correctness)
-        if(printData){
+        if (printData) {
             System.out.print("Input values: ");
-            for(int i=0;i<data.size(); i++)
+            for (int i = 0; i < data.size(); i++)
                 System.out.print(data.get(i) + " ");
             System.out.println();
         }
-        
+
         // Run one of the find functions; 
         // Check time before and after to measure runtime 
         long start = System.currentTimeMillis();
@@ -73,12 +74,13 @@ public class FindMultiples {
         int result = triplicates ? findTriplicate(data) : findDuplicate(data);
         long now = System.currentTimeMillis();
         double elapsed = (now - start) / 1000.0;
-        if(result >= 0)
+        if (result >= 0)
             System.out.println("Result: " + result + " (value: " + data.get(result) + ")");
         else
             System.out.println("Result: nothing found");
-        System.out.println("Elapsed time = " + elapsed + " seconds");        
+        System.out.println("Elapsed time = " + elapsed + " seconds");
 
     }
-  
+
 }
+
